9611bcf9e5b58b0c4fbeccd83e9c1bc8e6d98650
BbqGamer <korba.adam@gmail.com>
2024-04-03 18:11:32 +0200

    feat: Add marginal concordance calcualtion

--- electre_tri_b/concordance.py
+++ electre_tri_b/concordance.py
@@ -1,3 +1,31 @@
+GAIN = 0
+COST = 1
+
+
+def marginal_concordance(alternatives, boundary_profile, types, q, p):
+    res = []
+    for row in alternatives:
+        clist = []
+        for ga, ctype, gb, qi, pi in zip(row, types, boundary_profile, q, p):
+            if ctype == GAIN:
+                if ga - gb >= -qi:
+                    c = 1
+                elif ga - gb < -pi:
+                    c = 0
+                else:
+                    c = (pi - (gb - ga)) / (pi - qi)
+            else:
+                if ga - gb <= qi:
+                    c = 1
+                elif ga - gb > pi:
+                    c = 0
+                else:
+                    c = (pi - (ga - gb)) / (pi - qi)
+            clist.append(c)
+        res.append(clist)
+    return res
+
+
 def comprehensive_concordance(marginal_concordance, criteria_weights):
     return [
         sum(w * g for (w, g) in zip(criteria_weights, row)) / sum(criteria_weights)
--- electre_tri_b/concordance_test.py
+++ electre_tri_b/concordance_test.py
@@ -1,10 +1,21 @@
 import pytest
 
-from concordance import comprehensive_concordance
+from concordance import comprehensive_concordance, marginal_concordance
+
+
+def test_marginal_concordance():
+    A = [[90, 5], [40, 15], [60, 10]]
+    b = [64, 0]
+    types = [0, 1]
+    Q = [2, 5]
+    P = [6, 15]
+    expected = [[1, 1], [0, 0], [0.5, 0.5]]
+    res = marginal_concordance(A, b, types, Q, P)
+    assert res == expected
 
 
 def test_comprehensive_concordance():
     MC = [[0, 0, 0, 0], [1, 1, 1, 1], [0.5, 1, 0, 0], [1, 0.4, 1, 0]]
     W = [0.4, 0.3, 0.25, 0.05]
     res = comprehensive_concordance(marginal_concordance=MC, criteria_weights=W)
-    assert [0, 1, 0.5, 0.77] == pytest.approx(res, 0.2)
+    assert [0, 1, 0.5, 0.77] == pytest.approx(res)
